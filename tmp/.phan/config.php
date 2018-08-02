e4129764afe998356dc9a13e241e5bdda7cc2de2
raygu0101 <raygu0101@gmail.com>
2018-08-02 19:14:51 +0800

    [+]添加phan配置

---
+++ tmp/.phan/config.php
@@ -0,0 +1,19 @@
+<?php
+
+return \YunhanDev\Phan\PhanConfig::getConfig([
+    'suppress' => [
+    ],
+
+    'directory_list' => [
+        'app'
+    ],
+
+    'exclude_analysis_directory_list' => [
+    ],
+
+    'file_list' => [
+    ],
+
+    'exclude_file_list' => [
+    ]
+]);
